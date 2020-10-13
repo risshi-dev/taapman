6b142c67f2da0f92088ceceaecf15d765db611a9
Rishi Chitara <risshichitara@gmail.com>
2020-10-13 08:56:07 +0530

    Updates

---
+++ precache-manifest.b6481fa9fb321bcfac67b74d2c12ef7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6eae04a8d9f80ffda5d3341e20dfb1",
+    "url": "/taapman/index.html"
+  },
+  {
+    "revision": "8d70cd3df70cb45acd79",
+    "url": "/taapman/static/css/main.6bf74ff9.chunk.css"
+  },
+  {
+    "revision": "46150e1d8f2b203f4766",
+    "url": "/taapman/static/js/2.9968a9db.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/taapman/static/js/2.9968a9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d70cd3df70cb45acd79",
+    "url": "/taapman/static/js/main.d1d4e0f6.chunk.js"
+  },
+  {
+    "revision": "2cea26768c18ecb9b6c7",
+    "url": "/taapman/static/js/runtime-main.aade69ac.js"
+  }
+]);
